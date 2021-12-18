fa58aff54cb19e5d5548d4de0a9fc06b4392a692
ihfazhillah <mihfazhillah@gmail.com>
2021-12-18 23:15:41 +0700

    on click book, open new book

--- app/src/main/java/com/ihfazh/ksatriamuslim/adapters/BookRecyclerViewAdapter.kt
+++ app/src/main/java/com/ihfazh/ksatriamuslim/adapters/BookRecyclerViewAdapter.kt
@@ -2,15 +2,20 @@ package com.ihfazh.ksatriamuslim.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.ihfazh.ksatriamuslim.databinding.BookItemBinding
 import com.ihfazh.ksatriamuslim.domain.Book
+import com.ihfazh.ksatriamuslim.fragments.HomeFragmentDirections
 
 class BookViewHolder(private val binding: BookItemBinding): RecyclerView.ViewHolder(binding.root) {
     fun bind(book: Book) {
         binding.title.text = book.title
+        binding.root.setOnClickListener{
+            val action = HomeFragmentDirections.actionHomeFragmentToReaderFragment(book.id)
+            it.findNavController().navigate(action)
+        }
     }
-
 }
 
 class BookRecyclerViewAdapter: RecyclerView.Adapter<BookViewHolder>() {
