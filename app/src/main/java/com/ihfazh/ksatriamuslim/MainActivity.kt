6523fd13640070ba2450a186e93279c93d7d8ebb
ihfazhillah <mihfazhillah@gmail.com>
2021-12-18 12:01:54 +0700

    Bismillah, Initiate app

---
+++ app/src/main/java/com/ihfazh/ksatriamuslim/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.ihfazh.ksatriamuslim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.setupWithNavController(navController)
+    }
+}
