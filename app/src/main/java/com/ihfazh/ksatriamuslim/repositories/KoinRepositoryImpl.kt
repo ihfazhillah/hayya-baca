f4a6b898657a39b654f868aac7f4120008cfbdea
ihfazhillah <mihfazhillah@gmail.com>
2021-12-25 20:50:17 +0700

    Use firestore to save coin

--- app/src/main/java/com/ihfazh/ksatriamuslim/repositories/KoinRepositoryImpl.kt
+++ app/src/main/java/com/ihfazh/ksatriamuslim/repositories/KoinRepositoryImpl.kt
@@ -1,24 +1,78 @@
 package com.ihfazh.ksatriamuslim.repositories
 
 import android.content.Context
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import com.ihfazh.ksatriamuslim.domain.Koin
+import com.ihfazh.ksatriamuslim.remote.FirestoreService
+import com.ihfazh.ksatriamuslim.workers.UpdateFirestoreCoin
 
 class KoinRepositoryImpl(
     private val context: Context
 ): KoinRepository {
+    private val firestoreService = FirestoreService()
+
+    // TODO: ada isiannya
+    private val childId = "sakinah"
+
     override suspend fun getMine(): Koin {
         val sharedPreference = context.getSharedPreferences("ksatriamuslim", Context.MODE_PRIVATE)
         val myCoin = sharedPreference.getInt("my-coin", 0)
-        println("KOIN KU $myCoin")
-        return Koin(myCoin)
+
+        val id = getChildId(childId)
+
+        val remoteCoin = id?.let {
+            firestoreService.getFirestoreCoinById(it)
+        }
+
+        return Koin(remoteCoin ?: myCoin)
     }
+
 
     override suspend fun increaseMine() {
         val myCoin = getMine()
-        val sharedPreference = context.getSharedPreferences("ksatriamuslim", Context.MODE_WORLD_READABLE)
-        with(sharedPreference.edit()){
+        val finalCoin = myCoin.balance + 1
+        val id = getChildId(childId)
+
+        val updateSuccess = id?.let {
+            firestoreService.updateFireStoreById(id, finalCoin)
+        } ?: false
+
+        if (!updateSuccess) {
+            val request = OneTimeWorkRequestBuilder<UpdateFirestoreCoin>()
+                .setInputData(
+                    workDataOf(
+                        "id" to id,
+                        "coin" to finalCoin
+                    )
+                )
+                .build()
+
+            WorkManager.getInstance(context)
+                .enqueue(request)
+        }
+
+        val sharedPreference =
+            context.getSharedPreferences("ksatriamuslim", Context.MODE_WORLD_READABLE)
+        with(sharedPreference.edit()) {
             putInt("my-coin", myCoin.balance + 1)
             apply()
         }
     }
+
+    private suspend fun getChildId(username: String): String? {
+        val sharedPreference = context.getSharedPreferences("ksatriamuslim", Context.MODE_PRIVATE)
+        val id = sharedPreference.getString("child_id", null)
+        return id ?: firestoreService.getChildCoinIdByChild(username).also {
+            it?.let {
+                with(sharedPreference.edit()) {
+                    putString("child_id", it)
+                    apply()
+                }
+            }
+
+        }
+    }
+
 }
---
+++ app/src/main/java/com/ihfazh/ksatriamuslim/workers/UpdateFirestoreCoin.kt
@@ -0,0 +1,27 @@
+package com.ihfazh.ksatriamuslim.workers
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.ihfazh.ksatriamuslim.remote.FirestoreService
+import kotlinx.coroutines.*
+
+class UpdateFirestoreCoin(appContext: Context, workerParams: WorkerParameters) :
+    CoroutineWorker(appContext, workerParams) {
+    override suspend fun doWork(): Result {
+        val coin = inputData.getInt("coin", -1)
+        val id = inputData.getString("id") ?: return Result.failure()
+
+        if (coin < 0) {
+            return Result.failure()
+        }
+
+        return withContext(Dispatchers.IO) {
+            val firestoreService = FirestoreService()
+            val updateStatus = firestoreService.updateFireStoreById(id, coin)
+
+            return@withContext if (updateStatus) Result.success() else Result.retry()
+
+        }
+    }
+}
