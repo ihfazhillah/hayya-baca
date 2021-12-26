60b0296a5af939d7d9d42c6bee6f3728fa82542e
ihfazhillah <mihfazhillah@gmail.com>
2021-12-26 13:00:09 +0700

    Implement force update audio when notification received

--- app/src/main/java/com/ihfazh/ksatriamuslim/workers/ForceUpdateAllData.kt
+++ app/src/main/java/com/ihfazh/ksatriamuslim/workers/ForceUpdateAllData.kt
@@ -7,6 +7,8 @@ import com.ihfazh.ksatriamuslim.local.AppDatabase
 import com.ihfazh.ksatriamuslim.remote.Client
 import com.ihfazh.ksatriamuslim.repositories.BookRepositoryImpl
 import com.ihfazh.ksatriamuslim.repositories.ReadingBackgroundRepositoryImpl
+import com.ihfazh.ksatriamuslim.repositories.SpeakWordRepository
+import com.ihfazh.ksatriamuslim.repositories.SpeakWordRepositoryImpl
 import kotlinx.coroutines.*
 
 class ForceUpdateAllData(appContext: Context, workerParams: WorkerParameters) :
@@ -21,18 +23,23 @@ class ForceUpdateAllData(appContext: Context, workerParams: WorkerParameters) :
 
             val bookRepository = BookRepositoryImpl(local, remote)
             val backgroundRepository = ReadingBackgroundRepositoryImpl(local, remote)
+            val audioRepository: SpeakWordRepository =
+                SpeakWordRepositoryImpl(applicationContext, remote)
 
             when (type) {
                 "all" -> {
                     bookRepository.getBooksSummary(forceFetch = true)
                     backgroundRepository.getBackgrounds(forceFetch = true)
+                    audioRepository.saveAudios()
                 }
                 "onlyBook" -> {
                     bookRepository.getBooksSummary(forceFetch = true)
-                    println("only update book")
                 }
                 "onlyBackground" -> {
                     backgroundRepository.getBackgrounds(forceFetch = true)
+                }
+                "onlyAudios" -> {
+                    audioRepository.saveAudios()
                 }
             }
 
