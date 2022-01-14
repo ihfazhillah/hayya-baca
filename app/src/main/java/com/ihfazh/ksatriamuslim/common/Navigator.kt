72fcea6c8434d48ba3d854d3bb2ca6d63728b91e
ihfazhillah <mihfazhillah@gmail.com>
2022-01-14 22:27:12 +0700

    fix back to home, don't wait until the stopping recognizer done

--- app/src/main/java/com/ihfazh/ksatriamuslim/common/Navigator.kt
+++ app/src/main/java/com/ihfazh/ksatriamuslim/common/Navigator.kt
@@ -4,11 +4,12 @@ import android.view.View
 import androidx.lifecycle.LifecycleCoroutineScope
 import androidx.navigation.findNavController
 import com.ihfazh.ksatriamuslim.repositories.KoinRepositoryImpl
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class Navigator(val view: View?, private val lifecycleCoroutineScope: LifecycleCoroutineScope) {
     fun goHome(){
-        lifecycleCoroutineScope.launch {
+        lifecycleCoroutineScope.launch(Dispatchers.IO) {
             Recognizer.stopRecognizing()
         }
         view?.findNavController()?.navigateUp()
