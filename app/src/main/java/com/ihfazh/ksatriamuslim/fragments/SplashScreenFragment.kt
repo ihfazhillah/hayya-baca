68d0b3287a22ab04061b6567f48d65bab93bdad7
ihfazhillah <mihfazhillah@gmail.com>
2022-03-12 23:05:24 +0700

    add splash screen

--- app/src/main/java/com/ihfazh/ksatriamuslim/fragments/SplashScreenFragment.kt
+++ app/src/main/java/com/ihfazh/ksatriamuslim/fragments/SplashScreenFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.ihfazh.ksatriamuslim.R
+import com.ihfazh.ksatriamuslim.repositories.AuthenticationRepository
+import com.ihfazh.ksatriamuslim.repositories.GoogleAuthenticationRepositoryImpl
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +24,7 @@ class SplashScreenFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var authRepository: AuthenticationRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +40,16 @@ class SplashScreenFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        authRepository = GoogleAuthenticationRepositoryImpl(requireContext())
+        if (authRepository.isLoggedIn()){
+            findNavController().navigate(SplashScreenFragmentDirections.actionSplashScreenFragmentToHomeFragment())
+        } else {
+            findNavController().navigate(SplashScreenFragmentDirections.actionSplashScreenFragmentToLoginFragment())
+        }
     }
 
     companion object {
