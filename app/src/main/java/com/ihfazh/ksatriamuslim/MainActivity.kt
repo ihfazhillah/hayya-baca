224235a8883dc72b6add20dfaa051e32455dae2f
ihfazhillah <mihfazhillah@gmail.com>
2022-03-13 22:05:17 +0700

    Add parent gate page

--- app/src/main/java/com/ihfazh/ksatriamuslim/MainActivity.kt
+++ app/src/main/java/com/ihfazh/ksatriamuslim/MainActivity.kt
@@ -60,10 +60,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.main_menu, menu)
+//        return super.onCreateOptionsMenu(menu)
+//    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         navController.navigate(item.itemId)
