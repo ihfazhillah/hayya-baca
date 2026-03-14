451924191d24a7db7e91df45ce49d73e88d6ef26
Ihfazhillah <mihfazhillah@gmail.com>
2026-03-14 14:06:49 +0700

    Bump version to 1.0.9

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Hayya Baca",
   slug: "hayya-baca",
-  version: "1.0.8",
+  version: "1.0.9",
   orientation: "default",
   icon: "./assets/icon.png",
   userInterfaceStyle: "light",
