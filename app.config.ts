92f27a4f6be24c2b5075dc43e348ec81a309fabd
Ihfazhillah <mihfazhillah@gmail.com>
2026-04-12 16:37:40 +0700

    chore: bump version to 1.2.0

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Hayya Baca",
   slug: "hayya-baca",
-  version: "1.1.4",
+  version: "1.2.0",
   orientation: "default",
   icon: "./assets/icon.png",
   userInterfaceStyle: "light",
